5bcacd81a62033e6a132431c9c2f480dc8618e4f
cam-wil <cam-wil91@gmail.com>
2021-12-14 00:06:52 -0600

    initial pointers

---
+++ pointers_and_errors/go.mod
@@ -0,0 +1,3 @@
+module learn-go-with-tests/pointers_and_errors
+
+go 1.17
---
+++ pointers_and_errors/wallet.go
@@ -0,0 +1,16 @@
+package pointers_and_errors
+
+import "fmt"
+
+type Wallet struct {
+	balance int
+}
+
+func (w *Wallet) Deposit(amount int) {
+	fmt.Printf("address of balance in deposit is %v \n", &w.balance)
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() int {
+	return w.balance
+}
